73f357a6ec9c593969bdef362198326ee06987f9
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2022-02-06 11:23:46 +0200

    Adding Navbar and Announcement

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Announcement from "../components/Announcement";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Announcement />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
