ffdddf28b4bac26ebe25cf1faa43bc95d8b620a3
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2022-02-09 09:39:07 +0200

    Adding categories

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
 import Slider from "../components/Slider";
+import Categories from "../components/Categories";
 
 const Home = () => {
   return (
@@ -8,6 +9,7 @@ const Home = () => {
       <Announcement />
       <Navbar />
       <Slider />
+      <Categories />
     </div>
   );
 };
