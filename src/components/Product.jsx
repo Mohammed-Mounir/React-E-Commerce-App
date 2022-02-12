7c7adf8f2e985c81a90e1ac4baf3e91ad902d566
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2022-02-12 12:47:49 +0200

    Hoveriing over products changes opacity

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,6 +3,22 @@ import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 
+const Info = styled.div`
+  opacity: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  z-index: 3;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  transition: all 0.5s ease;
+`;
+
 const Container = styled.div`
   flex: 1;
   margin: 5px;
@@ -13,6 +29,10 @@ const Container = styled.div`
   justify-content: center;
   background-color: #f5fbfd;
   position: relative;
+
+  &:hover ${Info} {
+    opacity: 1;
+  }
 `;
 
 const Circle = styled.div`
@@ -26,20 +46,6 @@ const Circle = styled.div`
 const Image = styled.img`
   height: 75%;
   z-index: 2;
-`;
-
-const Info = styled.div`
-  opacity: 0;
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 3;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  background-color: rgba(0, 0, 0, 0.2);
 `;
 
 const Icon = styled.div`
