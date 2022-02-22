64b911a29bffb50b4389fae6a548270ea668978b
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2022-02-22 09:32:51 +0200

    MongoDB connection configuration

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-app.listen(5000, () => console.log("Server is running..."));
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successful!"))
+  .catch((err) => console.log(err));
+
+app.listen(process.env.PORT || 5000, () => console.log("Server is running..."));
