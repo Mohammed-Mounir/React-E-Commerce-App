c8f40236ca6da443f90d265e660cfc97e7a466ee
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2022-02-23 08:02:09 +0200

    Added models and initial routes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const userRoute = require("./routes/user");
 
 dotenv.config();
 
@@ -9,5 +10,8 @@ mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("DB Connection Successful!"))
   .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, () => console.log("Server is running..."));
