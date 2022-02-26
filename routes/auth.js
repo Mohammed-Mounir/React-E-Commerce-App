0be317cab53a59550b4681e92b0e6db00bb2e18b
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2022-02-26 08:23:06 +0200

    Implemented NodeJS MongoDB authentication

---
+++ routes/auth.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+
+// Register
+router.post("/register", async (req, res) => {
+  const { username, email, password } = req.body;
+  const newUser = new User({
+    username,
+    email,
+    password: CryptoJS.AES.encrypt(password, process.env.PASS_SEC).toString(),
+  });
+
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong credentials!");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+    const userPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    userPassword !== req.body.password &&
+      res.status(401).json("Wrong credentials!");
+
+    const { password, ...others } = user._doc;
+
+    res.status(200).json(others);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
