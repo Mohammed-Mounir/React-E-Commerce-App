76bea63ebffeb213d3693d8cf2d1d7375c685c2d
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2022-03-20 19:38:35 +0200

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,30 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const App = () => {
-  return <Cart />;
+  const user = true;
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/products/:category" element={<ProductList />} />
+      <Route path="/Product/:id" element={<Product />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route
+        path="/login"
+        element={user ? <Navigate to="/" replace /> : <Login />}
+      />
+      <Route
+        path="/register"
+        element={user ? <Navigate to="/" replace /> : <Register />}
+      />
+    </Routes>
+  );
 };
 
 export default App;
