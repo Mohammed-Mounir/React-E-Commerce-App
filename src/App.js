43d4bea9049599e7aff7a21d9004c5c7598988f1
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2022-04-10 08:22:54 +0200

    Added sidebar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import Sidebar from "./components/Sidebar/Sidebar";
 import Topbar from "./components/Topbar/Topbar";
+import "./App.css";
 
 function App() {
   return (
     <div>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <main className="main">Main Content</main>
+      </div>
     </div>
   );
 }
