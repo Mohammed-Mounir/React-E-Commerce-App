478e7335cddc39401574da4bdb23b11c6ac5f3bf
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2022-04-11 09:06:12 +0200

    Added reusable chart component

---
+++ src/dummyData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active User": 4000,
+  },
+  {
+    name: "Feb",
+    "Active User": 3000,
+  },
+  {
+    name: "Mar",
+    "Active User": 2000,
+  },
+  {
+    name: "Apr",
+    "Active User": 2780,
+  },
+  {
+    name: "May",
+    "Active User": 1890,
+  },
+  {
+    name: "Jun",
+    "Active User": 2390,
+  },
+  {
+    name: "Jul",
+    "Active User": 3490,
+  },
+  {
+    name: "Aug",
+    "Active User": 5624,
+  },
+  {
+    name: "Sep",
+    "Active User": 2305,
+  },
+  {
+    name: "Oct",
+    "Active User": 3426,
+  },
+  {
+    name: "Nov",
+    "Active User": 6020,
+  },
+  {
+    name: "Dec",
+    "Active User": 2406,
+  },
+];
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
 import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
+import Chart from "../../components/Chart/Chart";
+import { userData } from "../../dummyData";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
+      <Chart
+        data={userData}
+        title="User Analytics"
+        grid
+        dataKey="Active User"
+      />
     </div>
   );
 };
