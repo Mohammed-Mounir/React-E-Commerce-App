3698668faebf20f3a0ad429392acecd149aee3b6
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2022-04-11 08:34:19 +0200

    Added featured info component

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,12 @@
+import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
 import "./Home.css";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <FeaturedInfo />
+    </div>
+  );
 };
 
 export default Home;
