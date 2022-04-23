a2d8652a5024bc71b44c4cc19b3e881d13787049
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2022-04-23 16:21:25 +0200

    updating widgetLg component to fetch orders

--- src/components/WidgetLg/WidgetLg.jsx
+++ src/components/WidgetLg/WidgetLg.jsx
@@ -1,6 +1,19 @@
+import { useState, useEffect } from "react";
+import { userRequest } from "../../requestMethods";
+import { format } from "timeago.js";
 import "./WidgetLg.css";
 
 const WidgetLg = () => {
+  const [orders, setOrders] = useState([]);
+
+  useEffect(() => {
+    const getOrders = async () => {
+      const res = await userRequest.get("orders");
+      setOrders(res.data);
+    };
+    getOrders();
+  }, []);
+
   const Button = ({ type }) => {
     return <button className={`widgetLgButton ${type}`}>{type}</button>;
   };
@@ -8,72 +21,28 @@ const WidgetLg = () => {
     <div className="widgetLg">
       <h3 className="widgetLgTitle">Latest transactions</h3>
       <table className="widgetLgTable">
-        <tr className="widgetLgtr">
-          <th className="widgetLgTh">Customer</th>
-          <th className="widgetLgTh">Date</th>
-          <th className="widgetLgTh">Amount</th>
-          <th className="widgetLgTh">Status</th>
-        </tr>
-        <tr className="widgetLgtr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Keanu Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Approved" />
-          </td>
-        </tr>
-        <tr className="widgetLgtr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Keanu Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Declined" />
-          </td>
-        </tr>
-        <tr className="widgetLgtr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Keanu Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Pending" />
-          </td>
-        </tr>
-        <tr className="widgetLgtr">
-          <td className="widgetLgUser">
-            <img
-              src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-              alt=""
-              className="widgetLgImg"
-            />
-            <span className="widgetLgName">Keanu Carol</span>
-          </td>
-          <td className="widgetLgDate">2 Jun 2021</td>
-          <td className="widgetLgAmount">$122.00</td>
-          <td className="widgetLgStatus">
-            <Button type="Approved" />
-          </td>
-        </tr>
+        <thead>
+          <tr className="widgetLgtr">
+            <th className="widgetLgTh">Customer</th>
+            <th className="widgetLgTh">Date</th>
+            <th className="widgetLgTh">Amount</th>
+            <th className="widgetLgTh">Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order) => (
+            <tr key={order._id} className="widgetLgtr">
+              <td className="widgetLgUser">
+                <span className="widgetLgName">{order.userId}</span>
+              </td>
+              <td className="widgetLgDate">{format(order.createdAt)}</td>
+              <td className="widgetLgAmount">${order.amount}</td>
+              <td className="widgetLgStatus">
+                <Button type={order.status} />
+              </td>
+            </tr>
+          ))}
+        </tbody>
       </table>
     </div>
   );
