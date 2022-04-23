b49f1351640a1a4d6b3b886b23e38cfae27f173a
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2022-04-23 15:25:57 +0200

    retrieving token from local storage

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5000/api/";
-const TOKEN =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyMWM2MWZlNTUxYWZhYzQxYTdiZDQ5YyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0ODIzNzg2NCwiZXhwIjoxNjQ4NDk3MDY0fQ.9I_DtBS4lgb0-dLq_TBlHd7hbscTSf_cEiN1J8qXgPM";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
+  .currentUser.accessToken;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
