50071e829aea9187b12bcd8a9dea353279e0974f
Mohammed-Mounir <egy.mohammedmounir@gmail.com>
2022-05-01 20:41:28 +0200

    updated income route

--- routes/order.js
+++ routes/order.js
@@ -65,15 +65,33 @@ router.get("/", verifyTokenAndAdmin, async (req, res) => {
 
 // GET MONTHLY INCOME
 router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+  const productId = req.query.pid;
   const date = new Date();
   const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
   const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
 
   try {
     const income = await Order.aggregate([
-      { $match: { createdAt: { $gte: previousMonth } } },
-      { $project: { month: { $month: "$createdAt" }, sales: "$amount" } },
-      { $group: { _id: "$month", total: { $sum: "$sales" } } },
+      {
+        $match: {
+          createdAt: { $gte: previousMonth },
+          ...(productId && {
+            products: { $elemMatch: { productId } },
+          }),
+        },
+      },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+          sales: "$amount",
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: "$sales" },
+        },
+      },
     ]);
     res.status(200).json(income);
   } catch (err) {
